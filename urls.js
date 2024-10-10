340419c063136abd3688ae7f7f5b31c13188a4d0
RalphsCode <RalphsCode@gmail.com>
2024-10-10 15:16:38 -0700

    Parsing out the info from the source file

--- urls.js
+++ urls.js
@@ -1,35 +1,55 @@
 const fs = require('fs');
-
+// Get the paramaters from the command line
 const args = process.argv;
+// get the filename from the array
 const filename = args[2];
 
 async function readFile(filename) {
+    /* get the data from the passed in filename */
     try {
         const data = await fs.promises.readFile(filename, 'utf8'); 
 
-        const lines = data.split('\n');
-        
-        let urlList = [];
-        for (let line of lines) {
-            urlList.push(line.trim());
-            }
-        return urlList;
+        return data;
 
         } catch (err) {
             // Handle possible error
-            console.error(err);
+            console.error(" <<< There was an error retrieving the data >>>\n", err);
             process.exit(1); // Exit the process with an error code
         }
         }  // END readFIle()
 
-async function main() {
+function writeFile(urls) {
+    // Extract the url & domain from each URL
+    for (let url of urls) {
+        const sourceURL = url;
+        const fileName = new URL(url).hostname;
+        // const  = fullURL.hostname;
+        console.log("fileName:", fileName);
+        console.log("source:", sourceURL);
+    }
+    return true;
+    
+}
 
-        const urlList = await readFile(filename);
-        let count = 1;
-        for (url of urlList)  {
-            console.log(count, url);
-            count++;
-        }
+async function main() {
+        // request the contents of the passed in file
+        const urlStr = await readFile(filename);
+
+        // Regular expression to match URLs
+        const urlRegex = /(https?:\/\/[^\s]+)/g;
+
+        // Extract URLs from the string
+        const urls = urlStr.match(urlRegex);
+
+        console.log("URL's:", urls)
+
+        if (urls) {
+            writeFile(urls)
+        } else {
+            console.log("No domains were found.")
+        };
+        
+
 } // END main()
   
 main();
