1fdfa4215196883dcd8d94f9dec78b897c94c435
RalphsCode <RalphsCode@gmail.com>
2024-10-11 11:45:30 -0700

    Broken App fixed, and issues documented.

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,32 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
 
-app.post('/', function(req, res, next) {
+// Middleware to use JSON
+app.use(express.json()); 
+
+/* Display the GitHub users matching the usernames passed in via the body of the send request. */
+app.post('/', async function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    // get the GitHub usernames from the body of the send request
+    const results = await Promise.all(
+      req.body.developers.map(async developer => {
+      return await axios.get(`https://api.github.com/users/${developer}`);
+    }) );
 
-    return res.send(JSON.stringify(out));
-  } catch {
+    // Loop through the array and present the user name and bio
+    const response = results.map(user => ({ name: user.data.name, bio: user.data.bio }));
+  
+
+    return res.json(response);
+
+  } catch(err) {
     next(err);
   }
 });
 
-app.listen(3000);
+// Initiate Server Instance
+app.listen(3000, () => {
+  console.log("Server is running & listening on port 3000.");
+});
+
--- broken-app/issues.md
+++ broken-app/issues.md
@@ -1,1 +1,19 @@
 # Broken App Issues
+
+app and axios should be defined with const - so it cannot be reassigned, and so that its value cannot be changed.
+
+Creating the server should have a callback message indicating that the server is running.
+
+There are no comments in the code. Added some.
+
+The variable names are not descriptive nor helpful. Changed 'out' to response, d to developer and r to user.
+
+Add err argument to catch.
+
+Add Middleware to use JSON.
+
+Change the syntax of out to use JSON in Express.
+
+Change the route function to be Async.
+
+There should also be error catching or try/catch for the api calls, to handle if a username does not exist on GitHub. But I don't have the time to add it, and its not a requirement per the specs.
